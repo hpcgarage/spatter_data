ac71e86f327fa2932d6069f741f36453a2c182bf
Patrick Lavin <plavin3@gatech.edu>
2019-04-09 09:28:04 -0400

    bugfix

--- scripts/traces.sh
+++ scripts/traces.sh
@@ -36,10 +36,10 @@ for i in `seq 1 10`;
 do 
 	OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $nekbone_size -t $nekbone_trace -q --nph -R 20 | grep OPENMP > tmp1.txt
 	tail -n +2 papi_output.txt > tmp2.txt
-	pr -w 180 -m -t tmp1.txt tmp2.txt > tmp3.txt
+	pr -w 180 -m -t tmp1.txt tmp2.txt >> tmp3.txt
 	#OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t ../traces/amg.pat -q --nph -R 20 | grep OPENMP | cut -f 10 -d' ' | sort -r | head -n 1 >> tmp2.txt
 done;
-
+cat tmp3.txt
 echo -n "NEKBONE $nekbone_size PAPI01 " >> $outfile
 cat tmp3.txt | sort -nrk10 | head -n 1 | cut -f 4,10,11,12 -d' ' >> $outfile
 
@@ -50,37 +50,38 @@ for i in `seq 1 10`;
 do 
 	OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $nekbone_size -t $nekbone_trace -q --nph -R 20 | grep OPENMP > tmp1.txt
 	tail -n +2 papi_output.txt > tmp2.txt
-	pr -w 180 -m -t tmp1.txt tmp2.txt > tmp3.txt
+	pr -w 180 -m -t tmp1.txt tmp2.txt >> tmp3.txt
 	#OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t ../traces/amg.pat -q --nph -R 20 | grep OPENMP | cut -f 10 -d' ' | sort -r | head -n 1 >> tmp2.txt
 done;
 
 echo -n "NEKBONE $nekbone_size PAPI23 "  >> $outfile
 cat tmp3.txt | sort -nrk10 | head -n 1 | cut -f 4,10,11,12 -d' ' >>$outfile
+rm -f $tmpfiles
 
 cp papi_config_456.txt papi_config.txt
 for i in `seq 1 10`; 
 do 
 	OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $nekbone_size -t $nekbone_trace -q --nph -R 20 | grep OPENMP > tmp1.txt
 	tail -n +2 papi_output.txt > tmp2.txt
-	pr -w 180 -m -t tmp1.txt tmp2.txt > tmp3.txt
+	pr -w 180 -m -t tmp1.txt tmp2.txt >> tmp3.txt
 	#OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t ../traces/amg.pat -q --nph -R 20 | grep OPENMP | cut -f 10 -d' ' | sort -r | head -n 1 >> tmp2.txt
 done;
 
 echo -n "NEKBONE $nekbone_size PAPI456 " >> $outfile
 cat tmp3.txt | sort -nrk10 | head -n 1 | cut -f 4,10,11,12,13 -d' ' >> $outfile
+rm -f $tmpfiles
 
 cp papi_config_01.txt papi_config.txt
 for i in `seq 1 10`; 
 do 
 	OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t $amg_trace -q --nph -R 20 | grep OPENMP > tmp1.txt
 	tail -n +2 papi_output.txt > tmp2.txt
-	pr -w 180 -m -t tmp1.txt tmp2.txt > tmp3.txt
+	pr -w 180 -m -t tmp1.txt tmp2.txt >> tmp3.txt
 	#OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t ../traces/amg.pat -q --nph -R 20 | grep OPENMP | cut -f 10 -d' ' | sort -r | head -n 1 >> tmp2.txt
 done;
 
 echo -n "AMG $amg_size PAPI01 " >> $outfile
 cat tmp3.txt | sort -nrk10 | head -n 1 | cut -f 4,10,11,12 -d' ' >> $outfile
-
 rm -f $tmpfiles
 
 cp papi_config_23.txt papi_config.txt
@@ -88,19 +89,20 @@ for i in `seq 1 10`;
 do 
 	OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t $amg_trace -q --nph -R 20 | grep OPENMP > tmp1.txt
 	tail -n +2 papi_output.txt > tmp2.txt
-	pr -w 180 -m -t tmp1.txt tmp2.txt > tmp3.txt
+	pr -w 180 -m -t tmp1.txt tmp2.txt >> tmp3.txt
 	#OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t ../traces/amg.pat -q --nph -R 20 | grep OPENMP | cut -f 10 -d' ' | sort -r | head -n 1 >> tmp2.txt
 done;
 
 echo -n "AMG $amg_size PAPI23 "  >> $outfile
 cat tmp3.txt | sort -nrk10 | head -n 1 | cut -f 4,10,11,12 -d' ' >> $outfile
+rm -f $tmpfiles
 
 cp papi_config_456.txt papi_config.txt
 for i in `seq 1 10`; 
 do 
 	OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t $amg_trace -q --nph -R 20 | grep OPENMP > tmp1.txt
 	tail -n +2 papi_output.txt > tmp2.txt
-	pr -w 180 -m -t tmp1.txt tmp2.txt > tmp3.txt
+	pr -w 180 -m -t tmp1.txt tmp2.txt >> tmp3.txt
 	#OMP_NUM_THREADS=1 likwid-pin -c N:0 ./spatter -l $amg_size -t ../traces/amg.pat -q --nph -R 20 | grep OPENMP | cut -f 10 -d' ' | sort -r | head -n 1 >> tmp2.txt
 done;
 
