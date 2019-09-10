83cc8636752e534a1cde7c69cdc216cf121017e9
Patrick Lavin <plavin3@gatech.edu>
2019-09-10 12:58:00 -0400

    fix app trace json

--- app_characterization/app_traces_json/convert_to_spatter.py
+++ app_characterization/app_traces_json/convert_to_spatter.py
@@ -13,5 +13,7 @@ if (len(sys.argv) != 2):
 configs = file_to_dict(sys.argv[1])
 for c in configs:
     c['pattern'] = [p//8 for p in c['pattern']]
+    for i in range(1,len(c['pattern'])):
+        c['pattern'][i] = c['pattern'][i] + c['pattern'][i-1]
     c['delta']   = c['delta'] // 8
 print(json.dumps(configs))
