ca416dc02ac23ab5880abced0e25217f98ef82a5
Patrick Lavin <plavin3@gatech.edu>
2019-09-11 12:06:11 -0400

    fix conversion from aarons format to spatter

--- app_characterization/app_traces_json/convert_to_spatter.py
+++ app_characterization/app_traces_json/convert_to_spatter.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import numpy as np
 
 def file_to_dict(filename):
     with open(filename, 'r') as file:
@@ -12,8 +13,9 @@ if (len(sys.argv) != 2):
     
 configs = file_to_dict(sys.argv[1])
 for c in configs:
-    c['pattern'] = [p//8 for p in c['pattern']]
-    for i in range(1,len(c['pattern'])):
-        c['pattern'][i] = c['pattern'][i] + c['pattern'][i-1]
+    ca = (np.array(c['pattern']) // 8).cumsum()
+    ca = ca - min(ca)
+    c['pattern'] = ca.tolist()
     c['delta']   = c['delta'] // 8
+       
 print(json.dumps(configs))
